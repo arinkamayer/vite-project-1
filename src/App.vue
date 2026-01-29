0cb6bd45e51d4cae5c240c841119a6b3b4e358eb
AriMay <e.miroshnikova@gt-sot.ru>
2026-01-29 22:20:48 +0300

    4.13.dz-components

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,17 @@
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from 'vue'
-
-const currentDate = ref(new Date())
-
-const formattedDate = computed(() => {
-  return currentDate.value.toLocaleDateString('ru-RU', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-    weekday: 'long'
-  })
-})
-
-const formattedTime = computed(() => {
-  return currentDate.value.toLocaleTimeString('ru-RU')
-})
-
-let intervalId = null
-
-onMounted(() => {
-  intervalId = setInterval(() => {
-    currentDate.value = new Date() 
-  }, 1000)
-})
-
-onUnmounted(() => {
-  if (intervalId) clearInterval(intervalId)
-})
+import Button from './components/Button.vue';
 </script>
 
 <template>
-  <div class="block">
-    <div class="header">Текущая дата и время:</div>
-    <div class="data">Дата: {{ formattedDate }}</div>
-    <div class="time">Время: {{ formattedTime }}</div>
-  </div>
+  <main class="start-game-btn">
+    <Button>Начать игру</Button>
+  </main>
 </template>
 
 <style scoped>
-.block {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.start-game-btn {
+  padding: 16px 10px;
+  border-radius: 100px;
+  gap: 10px;
 }
-.header {
-  font-size: 4rem;
-  font-weight: bold;
-}
-
-.data {
-  font-size: 2rem;
-  padding-top: 80px;
-}
-
-.time {
-  font-size: 2rem;
-  padding-top: 40px;
-}
-
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  border-color: #000000;
+  border-radius: 100px;
+  background: var(--color-bg-btn);
+  padding: 16px 10px;
+  font-family: var(--font);
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 36px;
+  color: var(--primary);
+  cursor: pointer;
+  width: 335px;
+  height: 68px;
+  box-shadow: 0px 4px 4px #00000040, 0px 4px 4px #00000040;
+}
+</style>
